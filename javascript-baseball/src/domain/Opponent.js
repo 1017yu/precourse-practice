d09586004aa4a061c7f3c9d30e0a14bc77fa70a6
Kled <1017yu@daum.net>
2023-11-26 12:54:00 +0900

    refactor: 불필요 private method를 public method에 병합

--- javascript-baseball/src/domain/Opponent.js
+++ javascript-baseball/src/domain/Opponent.js
@@ -2,11 +2,11 @@ import { Random } from '@woowacourse/mission-utils';
 import { BALL_COUNT } from '../constants/options.js';
 
 class Opponent {
+  /**
+   * 상대방의 정답 번호가 담긴 배열을 반환한다.
+   * @returns {number[]} 상대방(컴퓨터)의 번호가 담긴 배열
+   */
   getRandomNumberSet() {
-    return this.#createRandomNumberSet();
-  }
-
-  #createRandomNumberSet() {
     const randomNumberSet = [];
 
     while (randomNumberSet.length < BALL_COUNT.length) {
