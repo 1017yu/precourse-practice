c88d0bdf3d38530006a69a3ed5ed9bd2b34c818b
Kled <1017yu@daum.net>
2023-11-26 11:17:26 +0900

    feat: 볼카운트 출력 메소드 추가

--- javascript-baseball/src/views/OutputView.js
+++ javascript-baseball/src/views/OutputView.js
@@ -1,9 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
-import OUTPUT_MESSAGE from '../constants/OutputMessage.js';
+import OUTPUT_MESSAGE from '../constants/outputMessage.js';
 
 const OutputView = {
   printIntro() {
     Console.print(OUTPUT_MESSAGE.intro);
+  },
+
+  printBallCount(ballCount) {
+    Console.print(ballCount);
   },
 };
 
