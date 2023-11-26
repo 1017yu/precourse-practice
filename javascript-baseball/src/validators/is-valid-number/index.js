60427f971bd304238737ff110769926ad4a919af
Kled <1017yu@daum.net>
2023-11-26 12:25:00 +0900

    feat: 정수 판별 로직 추가

--- javascript-baseball/src/validators/is-valid-number/index.js
+++ javascript-baseball/src/validators/is-valid-number/index.js
@@ -1,5 +1,8 @@
 const isValidNumberSet = (input) => {
-  return ![...input].some((value) => Number.isNaN(Number(value)));
+  return input.every((value) => {
+    const parsedValue = Number(value);
+    return !Number.isNaN(parsedValue) && Number.isInteger(parsedValue);
+  });
 };
 
 export default isValidNumberSet;
