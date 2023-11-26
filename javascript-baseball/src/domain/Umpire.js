92f09ee29f8629d9b328117f5b7904bd66917ca5
Kled <1017yu@daum.net>
2023-11-26 11:17:07 +0900

    feat: 볼카운트와 플레이어 승리 도출 로직 추가

--- javascript-baseball/src/domain/Umpire.js
+++ javascript-baseball/src/domain/Umpire.js
@@ -1,11 +1,38 @@
 import ERROR_MESSAGES from '../constants/errorMessages.js';
+import { BALL_COUNT, NUMBER } from '../constants/options.js';
 import errorHandler from '../error/handler.js';
 import { isValidNumber, isValidLength } from '../validators/index.js';
 
 class Umpire {
   constructor(playerNumberSet) {
-    this.playerNumberSet = playerNumberSet;
+    this.playerNumberSet = Array.from(playerNumberSet, Number);
     this.#validate(this.playerNumberSet);
+  }
+
+  ballcount(opponentNumberSet) {
+    const [balls, strikes] = this.#calculateBallcount(opponentNumberSet);
+    if (!balls && !strikes) return `${BALL_COUNT.nothing}`;
+
+    return (
+      (balls ? `${balls}${BALL_COUNT.ball} ` : '') +
+      (strikes ? `${strikes}${BALL_COUNT.strike}` : '')
+    );
+  }
+
+  isPlayerWin(ballcount) {
+    return ballcount === `${NUMBER.length}${BALL_COUNT.strike}`;
+  }
+
+  #calculateBallcount(opponentNumberSet) {
+    let strikes = 0;
+    let balls = 0;
+
+    this.playerNumberSet.forEach((number, index) => {
+      if (number === opponentNumberSet[index]) return (strikes += 1);
+      opponentNumberSet.includes(number) ? (balls += 1) : 0;
+    });
+
+    return [balls, strikes];
   }
 
   #validate(playerNumberSet) {
