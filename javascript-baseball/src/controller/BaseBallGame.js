a7cbf1b0858a26488035f8b7a650944ef46cbec5
Kled <1017yu@daum.net>
2023-11-26 12:24:09 +0900

    feat: 숫자야구 종료 및 재시작 로직 추가

--- javascript-baseball/src/controller/BaseBallGame.js
+++ javascript-baseball/src/controller/BaseBallGame.js
@@ -2,6 +2,10 @@ import OutputView from '../views/OutputView.js';
 import InputView from '../views/InputView.js';
 import Opponent from '../domain/Opponent.js';
 import Umpire from '../domain/Umpire.js';
+import { GAME_OPTION } from '../constants/options.js';
+import errorHandler from '../error/handler.js';
+import { isValidRetryInput } from '../validators/index.js';
+import ERROR_MESSAGES from '../constants/errorMessages.js';
 
 class BaseBallGame {
   constructor() {
@@ -10,7 +14,9 @@ class BaseBallGame {
 
   async controll() {
     const opponentNumberSet = new Opponent().getRandomNumberSet();
+
     await this.#start(opponentNumberSet);
+    await this.#end();
   }
 
   async #start(opponentNumberSet) {
@@ -19,8 +25,18 @@ class BaseBallGame {
       const ballcount = umpire.ballcount(opponentNumberSet);
 
       OutputView.printBallCount(ballcount);
+
       if (umpire.isPlayerWin(ballcount)) break;
     }
+  }
+
+  async #end() {
+    OutputView.printOutro();
+
+    const retryInput = await InputView.retry();
+
+    if (!isValidRetryInput(retryInput)) errorHandler(ERROR_MESSAGES.retry);
+    if (retryInput === GAME_OPTION.retry) this.controll();
   }
 }
 
