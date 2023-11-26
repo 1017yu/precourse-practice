78141bd42eabbd7b1b4e49fe1bbb81806e0df9c4
Kled <1017yu@daum.net>
2023-11-26 11:16:39 +0900

    feat: 상대방(컴퓨터)의 무작위 수를 맞혀나가는 로직 추가

--- javascript-baseball/src/controller/BaseBallGame.js
+++ javascript-baseball/src/controller/BaseBallGame.js
@@ -1,11 +1,27 @@
 import OutputView from '../views/OutputView.js';
+import InputView from '../views/InputView.js';
+import Opponent from '../domain/Opponent.js';
+import Umpire from '../domain/Umpire.js';
 
 class BaseBallGame {
   constructor() {
     OutputView.printIntro();
   }
 
-  async controll() {}
+  async controll() {
+    const opponentNumberSet = new Opponent().getRandomNumberSet();
+    await this.#start(opponentNumberSet);
+  }
+
+  async #start(opponentNumberSet) {
+    while (true) {
+      const umpire = new Umpire(await InputView.number());
+      const ballcount = umpire.ballcount(opponentNumberSet);
+
+      OutputView.printBallCount(ballcount);
+      if (umpire.isPlayerWin(ballcount)) break;
+    }
+  }
 }
 
 export default BaseBallGame;
