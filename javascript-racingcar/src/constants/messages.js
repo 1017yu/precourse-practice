8ff8d694fce3724fa4c697288b1cc31f8a7a24aa
Kled <1017yu@daum.net>
2023-12-01 11:29:05 +0900

    feat: 시도할 횟수 메시지 상수 추가

--- javascript-racingcar/src/constants/messages.js
+++ javascript-racingcar/src/constants/messages.js
@@ -1,12 +1,16 @@
+import SYMBOLS from './symbols.js';
+
 export const OUTPUT = Object.freeze({
-  intro: '',
+  result: '실행 결과',
 });
 
 export const INPUT = Object.freeze({
-  carName: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`,
+  carName: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(${SYMBOLS.comma}) 기준으로 구분)\n`,
+  attempts: `시도할 횟수는 몇 회인가요?\n`,
 });
 
 export const ERROR = Object.freeze({
   prefix: '[ERROR]',
   nameLength: '자동차 이름은 5자 이하만 가능합니다.',
+  attempts: '시도할 횟수는 1 이상 숫자 형식만 가능합니다.',
 });
