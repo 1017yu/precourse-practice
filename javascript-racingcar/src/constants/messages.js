e7ee408bfe2c9aed36f0449de2107698c13631b9
Kled <1017yu@daum.net>
2023-12-01 10:59:31 +0900

    feat: 메시지 상수화

---
+++ javascript-racingcar/src/constants/messages.js
@@ -0,0 +1,12 @@
+export const OUTPUT = Object.freeze({
+  intro: '',
+});
+
+export const INPUT = Object.freeze({
+  carName: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`,
+});
+
+export const ERROR = Object.freeze({
+  prefix: '[ERROR]',
+  nameLength: '자동차 이름은 5자 이하만 가능합니다.',
+});
