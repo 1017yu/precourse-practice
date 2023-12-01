5d23ae43797c6d36212c6599cc14735ef618ff79
Kled <1017yu@daum.net>
2023-12-01 11:00:51 +0900

    feat: 자동차 이름 입력 유효성 검사 함수 추가

---
+++ javascript-racingcar/src/validators/index.js
@@ -0,0 +1,1 @@
+export { default as isValidCarName } from './is-valid-car-name/index.js';
---
+++ javascript-racingcar/src/validators/is-valid-car-name/index.js
@@ -0,0 +1,13 @@
+import { NAME_OPTIONS } from '../../constants/options.js';
+import SYMBOLS from '../../constants/symbols.js';
+
+const isValidCarName = (input) => {
+  const carNames = input.split(SYMBOLS.comma);
+
+  return carNames.every(
+    (carName) =>
+      carName.length <= NAME_OPTIONS.max && carName.length >= NAME_OPTIONS.min
+  );
+};
+
+export default isValidCarName;
