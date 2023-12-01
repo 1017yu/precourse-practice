efd0558423bfabaa5c9be34b2e967fe3c0bf7f16
Kled <1017yu@daum.net>
2023-12-01 13:15:38 +0900

    feat: 자동차 경주의 전진 로직 추가

--- javascript-racingcar/src/domain/Racing.js
+++ javascript-racingcar/src/domain/Racing.js
@@ -1,14 +1,34 @@
+import { Random } from '@woowacourse/mission-utils';
 import { ERROR } from '../constants/messages.js';
 import ErrorHandler from '../errors/handler.js';
 import { isValidAttempts } from '../validators/index.js';
 
 class Racing {
+  #scoreBoard = new Map();
+
   constructor(attempts) {
     this.#validate(attempts);
   }
 
+  move(carNames) {
+    carNames.forEach((carName) => {
+      const score = this.#getScore(carName);
+      this.#scoreBoard.set(carName, this.#isMovingFoward() ? score + 1 : score);
+    });
+
+    return this.#scoreBoard;
+  }
+
   #validate(attempts) {
     if (!isValidAttempts(attempts)) ErrorHandler(ERROR.attempts);
+  }
+
+  #isMovingFoward() {
+    return Random.pickNumberInRange(0, 9) >= 4;
+  }
+
+  #getScore(carName) {
+    return this.#scoreBoard.get(carName) ?? 0;
   }
 }
 
