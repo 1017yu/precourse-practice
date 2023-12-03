8bdbc5754ca1d99dc3a27ae0796c783535af4ce6
Kled <1017yu@daum.net>
2023-12-03 14:25:13 +0900

    feat: 로또 애플리케이션 엔트리 포인트 구현

--- javascript-lotto/src/App.js
+++ javascript-lotto/src/App.js
@@ -1,5 +1,19 @@
+import LottoController from './controller/LottoController.js';
+import Purchase from './models/Purchase.js';
+
 class App {
-  async play() {}
+  #controller;
+
+  #puchase;
+
+  constructor() {
+    this.#puchase = Purchase;
+    this.#controller = new LottoController(this.#puchase);
+  }
+
+  async play() {
+    await this.#controller.controll();
+  }
 }
 
 export default App;
