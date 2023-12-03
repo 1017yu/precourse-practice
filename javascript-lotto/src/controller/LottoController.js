bb4a10822326b373592f867aa03caec7f0fad8c8
Kled <1017yu@daum.net>
2023-12-03 18:00:51 +0900

    feat: 당첨 통계 로직 추가

--- javascript-lotto/src/controller/LottoController.js
+++ javascript-lotto/src/controller/LottoController.js
@@ -1,37 +1,82 @@
-import LottoGenerator from '../../utils/LottoGenerator.js';
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
+import lottoGenerator from '../utils/lottoGenerator.js';
+import SYMBOLS from '../constants/symbols.js';
+import BonusNumberValidator from '../validators/BonusNumber.js';
 
 class LottoController {
   #purchase;
 
-  constructor(purchase) {
+  #domain;
+
+  constructor(purchase, domain) {
     this.#purchase = purchase;
+    this.#domain = domain;
   }
 
   async controll() {
     await this.#readPurchaseAmount();
-    this.#printPurchasedLottos();
+    const lottos = this.#printPurchasedLottos();
+    const winningNumbers = await this.#readWinningNumbers(lottos);
+    const bonusNumber = await this.#readBonusNumber(winningNumbers);
+    this.#printStats(bonusNumber);
   }
 
   async #readPurchaseAmount() {
     try {
-      const purchaseAmountInput = await InputView.purchaseAmount();
-      this.#purchase = new this.#purchase(purchaseAmountInput);
+      const purchaseAmount = await InputView.purchaseAmount();
+
+      this.#purchase = new this.#purchase(purchaseAmount);
     } catch ({ message }) {
       OutputView.print(message);
-      await this.#readPurchaseAmount();
+      return await this.#readPurchaseAmount();
     }
   }
 
   #printPurchasedLottos() {
-    let purchaseAmount = this.#purchase.getPurchaseAmount();
-    OutputView.printPurchaseAmount(purchaseAmount);
+    const purchaseQuantity = this.#purchase.getPurchaseQuantity();
+    const lottos = lottoGenerator.getLottos(purchaseQuantity);
 
-    while (purchaseAmount) {
-      OutputView.print(LottoGenerator());
-      purchaseAmount -= 1;
+    OutputView.printPurchaseQuantity(purchaseQuantity);
+    OutputView.printLotto(lottos);
+
+    return lottos;
+  }
+
+  async #readWinningNumbers(lottos) {
+    const { comma } = SYMBOLS;
+    try {
+      const winningNumbers = (await InputView.winningNumbers()).split(comma);
+      this.#domain = new this.#domain(lottos, winningNumbers);
+
+      return winningNumbers;
+    } catch ({ message }) {
+      OutputView.print(message);
+      return await this.#readWinningNumbers();
     }
+  }
+
+  async #readBonusNumber(winningNumbers) {
+    try {
+      const bonusNumber = BonusNumberValidator(
+        await InputView.bonusNumber(),
+        winningNumbers
+      );
+
+      return bonusNumber;
+    } catch ({ message }) {
+      OutputView.print(message);
+      return await this.#readBonusNumber(winningNumbers);
+    }
+  }
+
+  #printStats(bonusNumber) {
+    const stats = this.#domain.getStats(bonusNumber);
+    const profitRatio = this.#domain.getProfitRatio(stats);
+
+    OutputView.printResult();
+    OutputView.printStats(stats);
+    OutputView.printProfitRatio(profitRatio);
   }
 }
 
