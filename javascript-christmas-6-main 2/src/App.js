f7e5e5fbaa2adec1079c4f98e9d192a8af872fe2
Kled <1017yu@daum.net>
2023-12-09 15:55:27 +0900

    feat: 12월 프로모션 App 구성

--- javascript-christmas-6-main 2/src/App.js
+++ javascript-christmas-6-main 2/src/App.js
@@ -1,5 +1,18 @@
+import Promotion from './controller/Promotion.js';
+import EventPlanner from './domain/EventPlanner.js';
+import Order from './domain/models/Order.js';
+import VisitDate from './domain/models/VisitDate.js';
+
 class App {
-  async run() {}
+  #controller;
+
+  constructor() {
+    this.#controller = new Promotion(EventPlanner, Order, VisitDate);
+  }
+
+  async run() {
+    await this.#controller.start();
+  }
 }
 
 export default App;
