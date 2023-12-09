f3db953a292670616f2b741c91b57b391358d6c1
Kled <1017yu@daum.net>
2023-12-09 15:57:24 +0900

    feat: string split 유틸 함수 추가

---
+++ javascript-christmas-6-main 2/src/utils/splitter.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {string} value split하고자 하는 대상
+ * @param {string} symbol split의 기준이 되는 심볼
+ * @returns {Array}
+ */
+const splitter = (value, symbol) => value.split(symbol);
+
+export default splitter;
