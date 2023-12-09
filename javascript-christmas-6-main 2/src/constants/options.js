c6aa7ba356bb1c57c68fabd40c96b2f0a07d3e69
Kled <1017yu@daum.net>
2023-12-09 16:15:01 +0900

    feat: 임계값 상수 추가

--- javascript-christmas-6-main 2/src/constants/options.js
+++ javascript-christmas-6-main 2/src/constants/options.js
@@ -1,10 +1,14 @@
-const OPTIONS = Object.freeze({
+export const OPTIONS = Object.freeze({
   startDate: 1,
   endDate: 31,
   christmas: 25,
   month: 12,
   intl: 'ko-KR',
   currency: '원',
+  none: '없음',
 });
 
-export default OPTIONS;
+export const THRESHOLDS = Object.freeze({
+  giveaway: 120_000,
+  event: 10_000,
+});
